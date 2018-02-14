c6f50866df9173809045abfe196c44acb9dd0ab0
Mitchell-Brown <mitrollbrown@yahoo.com>
2018-02-14 03:20:36 -0800

    initial upload

---
+++ Id/Modules/Test.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Discord;
+using Discord.Commands;
+using System.Threading.Tasks;
+
+namespace Id.Modules
+{
+    public class Test : ModuleBase<SocketCommandContext>
+    {
+
+        [Command("Test")]
+        public async Task skdfjasdkjf()
+        {
+            await Context.Channel.SendMessageAsync("Your actions have been recorded in the eternal book.");
+        }
+
+        [Command("user")]
+        public async Task test2lol(IGuildUser user) => await Context.Channel.SendMessageAsync(user.ToString());
+
+        [Command("roll")]
+        public async Task rollstuff(int1 int2)
+        {
+            await Context.Channel.SendMessageAsync("Rolling " + int1.toString + "d " + int2.toString);
+        }
+    }
+}
